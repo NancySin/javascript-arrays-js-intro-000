7559c2fa12365497bbd0a9344f5935421e3c4113
nancy sin <nsin@sbcglobal.net>
2018-01-02 23:53:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,17 +25,16 @@ function addElementToBeginningOfArray(array, element){
    array.shift();  // retrns removed element--> can't return array.shift()
     return array;
   }
-  
+
   function removeElementFromBeginningOfArray(array) {
    return array.slice(1);
   }
-  
+
   function destructivelyRemoveElementFromEndOfArray(array) {
    array.pop(); // returns removed element, so can't return array.pop()
     return array;
   }
-  
+
   function removeElementFromEndOfArray(array) {
     return array.slice(0, array.length-1);
   }
-  
